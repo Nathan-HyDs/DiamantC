c226f90395154643933cf6da7789ec8335c5ace4
Nathan-HyDs <pierre.rty@laposte.net>
2016-12-07 11:09:14 +0100

    encore need debuguage

--- diamantC/diamond.c
+++ diamantC/diamond.c
@@ -8,84 +8,84 @@ int nbConfigurations;
    functions to mangage the board
 ***********************************/
 board_t* createBoard() {
-    board_t* b = NULL;
-    board_t plateau;
-    b=&plateau;
-    clearBoard(b);
+    perror("createBoard");
+    board_t* plateau = malloc(sizeof(board_t));
+    clearBoard(plateau);
     for(int k=0;k<13;k++){
-        plateau.board[k]=NO_NEIGHBOR;
+        plateau->board[k]=NO_NEIGHBOR;
     }
 
     // define neighbors for cell 0
-    plateau.neighbors[0][0] = 1;
-    plateau.neighbors[0][1] = 3;
-    plateau.neighbors[0][2] = 4;
+    plateau->neighbors[0][0] = 1;
+    plateau->neighbors[0][1] = 3;
+    plateau->neighbors[0][2] = 4;
     // define neighbors for cell 1
-    plateau.neighbors[1][0] = 0;
-    plateau.neighbors[1][1] = 2;
-    plateau.neighbors[1][2] = 4;
-    plateau.neighbors[1][3] = 5;
+    plateau->neighbors[1][0] = 0;
+    plateau->neighbors[1][1] = 2;
+    plateau->neighbors[1][2] = 4;
+    plateau->neighbors[1][3] = 5;
     // define neighbors for cell 2
-    plateau.neighbors[2][0] = 1;
-    plateau.neighbors[2][1] = 5;
-    plateau.neighbors[2][2] = 6;
+    plateau->neighbors[2][0] = 1;
+    plateau->neighbors[2][1] = 5;
+    plateau->neighbors[2][2] = 6;
     // define neighbors for cell 3
-    plateau.neighbors[3][0] = 0;
-    plateau.neighbors[3][1] = 4;
-    plateau.neighbors[3][2] = 7;
+    plateau->neighbors[3][0] = 0;
+    plateau->neighbors[3][1] = 4;
+    plateau->neighbors[3][2] = 7;
     // define neighbors for cell 4
-    plateau.neighbors[4][0] = 0;
-    plateau.neighbors[4][1] = 1;
-    plateau.neighbors[4][2] = 3;
-    plateau.neighbors[4][3] = 5;
-    plateau.neighbors[4][4] = 7;
-    plateau.neighbors[4][5] = 8;
+    plateau->neighbors[4][0] = 0;
+    plateau->neighbors[4][1] = 1;
+    plateau->neighbors[4][2] = 3;
+    plateau->neighbors[4][3] = 5;
+    plateau->neighbors[4][4] = 7;
+    plateau->neighbors[4][5] = 8;
     // define neighbors for cell 5
-    plateau.neighbors[5][0] = 1;
-    plateau.neighbors[5][1] = 2;
-    plateau.neighbors[5][2] = 4;
-    plateau.neighbors[5][3] = 6;
-    plateau.neighbors[5][4] = 8;
-    plateau.neighbors[5][5] = 9;
+    plateau->neighbors[5][0] = 1;
+    plateau->neighbors[5][1] = 2;
+    plateau->neighbors[5][2] = 4;
+    plateau->neighbors[5][3] = 6;
+    plateau->neighbors[5][4] = 8;
+    plateau->neighbors[5][5] = 9;
     // define neighbors for cell 6
-    plateau.neighbors[6][0] = 2;
-    plateau.neighbors[6][1] = 5;
-    plateau.neighbors[6][2] = 9;
+    plateau->neighbors[6][0] = 2;
+    plateau->neighbors[6][1] = 5;
+    plateau->neighbors[6][2] = 9;
     // define neighbors for cell 7
-    plateau.neighbors[7][0] = 3;
-    plateau.neighbors[7][1] = 4;
-    plateau.neighbors[7][2] = 8;
-    plateau.neighbors[7][3] = 10;
+    plateau->neighbors[7][0] = 3;
+    plateau->neighbors[7][1] = 4;
+    plateau->neighbors[7][2] = 8;
+    plateau->neighbors[7][3] = 10;
     // define neighbors for cell 8
-    plateau.neighbors[8][0] = 4;
-    plateau.neighbors[8][1] = 5;
-    plateau.neighbors[8][2] = 7;
-    plateau.neighbors[8][3] = 9;
-    plateau.neighbors[8][4] = 10;
-    plateau.neighbors[8][5] = 11;
+    plateau->neighbors[8][0] = 4;
+    plateau->neighbors[8][1] = 5;
+    plateau->neighbors[8][2] = 7;
+    plateau->neighbors[8][3] = 9;
+    plateau->neighbors[8][4] = 10;
+    plateau->neighbors[8][5] = 11;
     // define neighbors for cell 9
-    plateau.neighbors[9][0] = 5;
-    plateau.neighbors[9][1] = 6;
-    plateau.neighbors[9][2] = 8;
-    plateau.neighbors[9][3] = 11;
+    plateau->neighbors[9][0] = 5;
+    plateau->neighbors[9][1] = 6;
+    plateau->neighbors[9][2] = 8;
+    plateau->neighbors[9][3] = 11;
     // define neighbors for cell 10
-    plateau.neighbors[10][0] = 7;
-    plateau.neighbors[10][1] = 8;
-    plateau.neighbors[10][2] = 11;
-    plateau.neighbors[10][3] = 12;
+    plateau->neighbors[10][0] = 7;
+    plateau->neighbors[10][1] = 8;
+    plateau->neighbors[10][2] = 11;
+    plateau->neighbors[10][3] = 12;
     // define neighbors for cell 11
-    plateau.neighbors[11][0] = 8;
-    plateau.neighbors[11][1] = 9;
-    plateau.neighbors[11][2] = 10;
-    plateau.neighbors[11][3] = 12;
+    plateau->neighbors[11][0] = 8;
+    plateau->neighbors[11][1] = 9;
+    plateau->neighbors[11][2] = 10;
+    plateau->neighbors[11][3] = 12;
     // define neighbors for cell 12
-    plateau.neighbors[12][0] = 10;
-    plateau.neighbors[12][1] = 11;
+    plateau->neighbors[12][0] = 10;
+    plateau->neighbors[12][1] = 11;
 
-    return b;
+    return plateau;
 }
 
 void clearBoard(board_t* b) {
+    perror("clearBoard");
     for(int i=0;i<13;i++)b->board[i]=VOID_CELL;
     b->blueScore=0;
     b->redScore=0;
@@ -93,12 +93,14 @@ void clearBoard(board_t* b) {
 }
 
 int voidCellIndex(board_t* b) {
+    perror("voidCellIndex");
     int id = -1;
     for(int i=0;i<13;i++)if(b->board[i]==VOID_CELL)id=i;
     return id;
 }
 
 void computeScore(board_t* b) {
+    perror("computeScore");
     b->blueScore=0;
     b->redScore=0;
 
@@ -106,17 +108,18 @@ void computeScore(board_t* b) {
 
     for(int i=0;i<6;i++){
         if(b->neighbors[idVoid][i] != NO_NEIGHBOR){
-            if(b->board[b->neighbors[idVoid][i]]<=6){
-                b->blueScore += b->board[b->neighbors[idVoid][i]];
+            if(b->board[(int)b->neighbors[idVoid][i]]<=6){
+                b->blueScore += b->board[(int)b->neighbors[idVoid][i]];
             }
             else{
-                b->redScore += (b->board[b->neighbors[idVoid][i]]-6);
+                b->redScore += (b->board[(int)b->neighbors[idVoid][i]]-6);
             }
         }
     }
 }
 
 void setPawn(board_t* b, int idCell, char value) {
+    perror("setPawn");
     b->board[idCell] = value;
 }
 
@@ -126,29 +129,36 @@ void setPawn(board_t* b, int idCell, char value) {
 ***********************************/
 
 node_t* createNode(int idCell, int turn) {
+    perror("createNode");
     node_t* n=malloc(sizeof(node_t));
 
     n->turn=turn;
     n->idCell=idCell;
 
-    node_t** children=NULL;
+    node_t** children;
+
     if (n->turn == 1){
-        node_t** children = malloc(sizeof(node_t)*1);
+        children = malloc(sizeof(node_t*)*1);
     }
     else if (n->turn < 12){
-        node_t** children = malloc(sizeof(node_t)*(13-turn));
+        children = malloc(sizeof(node_t*)*(13-turn));
+    }else{
+        children=NULL;
     }
 
     n->nbChildren=0;
     n->result=NO_RESULT;
     n->children=children;
+
     return n;
 }
 
 node_t* addChild(node_t* n, int idCell) {
+    perror("addChild");
     node_t* child = createNode(idCell, n->turn+1);
-    n->children[n->nbChildren]=child;
     n->nbChildren+=1;
+
+    n->children[(n->nbChildren)-1]=child;
     return child;
 }
 
@@ -157,73 +167,55 @@ node_t* addChild(node_t* n, int idCell) {
 ***********************************/
 
 tree_t* createTree() {
-
+    perror("createTree");
     tree_t* t= malloc(sizeof(tree_t));
-    node_t* root = malloc(sizeof(node_t));
+    //node_t* root = malloc(sizeof(node_t));
     /* A COMPLETER :
      - allouer l'espace mémoire pour un tree_t et mettre son adresse dans t
      - initialiser le champ root
   */
-    tree_t tree;
-    t = &tree;
-
-    /*************************
-        Definiton de root...
-  **************************/
-    node_t n;
-    n.idCell = 1;
-    n.turn = 0;
-    node_t** c=malloc(sizeof(node_t*));
-    n.children=c;
-    n.nbChildren=0;
-
-    tree.root=&n;
-
     return t;
 }
 
 
 void setFirstBlueChoice(tree_t* t, board_t* b, int idCell) {
     //A REFAIRE
+    perror("setFirstBlueChoice");
 /**************************
     Definition de node
 **************************/
-    node_t node;
-    node.idCell=idCell;
-    node.turn=1;
-
-    t->root=&node;
+    t->root=createNode(idCell,1);
     setPawn(b,idCell,1);
 
 }
 
 void setFirstRedChoice(tree_t* t, board_t* b, int idCell) {
     // A REFAIRE
+    perror("setFirstRedChoice");
 /**************************
     Definition de node
 **************************/
-    node_t node;
-    node.idCell=idCell;
-    node.turn=1;
-
-    t->root=&node;
-    setPawn(b,idCell,1);
+    t->root=createNode(idCell,1);
+    setPawn(b,idCell,7);
 
 }
 
 void buildTree(tree_t* t, board_t* b) {
+    perror("buildTree");
     /* A VERIFIER
   */
+
     nbConfigurations=5;
     node_t n;
     t->root->children[0]=&n;
     computePossibilities(&n,b);
+
 }
 
 void computePossibilities(node_t* n, board_t* b) {
-
+    perror("computePossibilites");
     if(n->turn == 12){
-        computeScore(b);
+        computeScore(b);//
         int red = b->redScore;
         int blue = b->blueScore;
         if(blue == red){
@@ -240,9 +232,23 @@ void computePossibilities(node_t* n, board_t* b) {
         return;
     }
 
+    char nextPawnValue= (n->turn+2)/2;
+    if((n->turn+1)%2==0){nextPawnValue+=6;}
+
+    for(int i=0;i<13;i++){
+        perror("ça passe");
+        if(b->board[i]==VOID_CELL){
+            setPawn(b,i,(int)nextPawnValue);
+            node_t* child = addChild(n,i);
+            computePossibilities(child,b);
+            setPawn(b,i,VOID_CELL);
+        }
+    }
+
 }
 
 int computeBlueVictories(node_t* n) {
+    perror("setFirstBlueVictories");
     int nb = 0;
     if (n->nbChildren==0){
         if(n->result==1){
@@ -250,7 +256,7 @@ int computeBlueVictories(node_t* n) {
         }
     }
     else {
-        for (int k=0 ; k < n ; k++){
+        for (int k=0 ; k < n->nbChildren ; k++){
             nb += computeBlueVictories(n->children[k]);
         }
     }
@@ -258,6 +264,8 @@ int computeBlueVictories(node_t* n) {
 }
 
 int computeRedVictories(node_t* n) {
+    perror("setFirstRedVictories");
+
     int nb = 0;
     if (n->nbChildren==0){
         if(n->result==2){
@@ -265,7 +273,7 @@ int computeRedVictories(node_t* n) {
         }
     }
     else {
-        for (int k=0 ; k < n ; k++){
+        for (int k=0 ; k < n->nbChildren ; k++){
             nb += computeBlueVictories(n->children[k]);
         }
     }
@@ -273,6 +281,8 @@ int computeRedVictories(node_t* n) {
 }
 
 int computeDraws(node_t* n) {
+    perror("computeDraws");
+
     int nb = 0;
     if (n->nbChildren==0){
         if(n->result==0){
@@ -280,7 +290,7 @@ int computeDraws(node_t* n) {
         }
     }
     else {
-        for (int k=0 ; k < n ; k++){
+        for (int k=0 ; k < n->nbChildren ; k++){
             nb += computeBlueVictories(n->children[k]);
         }
     }
--- diamantC/game.c
+++ diamantC/game.c
@@ -12,11 +12,17 @@ int main(int argc, char** argv){
 
   //Bug ici
   setFirstBlueChoice(t,b,idCellBlue);
+  perror("main-line-15");
   setFirstRedChoice(t,b,idCellRed);
+  perror("main-line-17");
   buildTree(t,b);
+  perror("main-line-19");
   int nbBlueVictories = computeBlueVictories(t->root);
+  perror("main-line-21");
   int nbRedVictories = computeRedVictories(t->root);
+  perror("main-line-22");
   int nbDraws = computeDraws(t->root);
+  perror("main-line-25");
   printf("nb configuration: %d, nb blue victories: %d, nb red victories: %d, nb draws: %d\n",nbConfigurations,nbBlueVictories, nbRedVictories, nbDraws);
   return 0;
 }
