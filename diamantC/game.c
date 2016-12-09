e825e01f8b7fb28bff6a8abdb65b80ab4c11f5b7
Nathan-HyDs <pierre.rty@laposte.net>
2016-12-09 20:26:23 +0100

    Fix probleme switch case

--- diamantC/game.c
+++ diamantC/game.c
@@ -155,7 +155,7 @@ int main(int argc, char** argv){
         do{
             printf("Choix :");
             scanf("%d",&choice);
-        }while(choice>4 && choice>=0);
+        }while(choice<4 && choice>=0);
 
         switch(choice){
             case 1:
